654b829ef1b8c81d025e9545624f128f108f9723
Huw Day <85741581+HuwWDay@users.noreply.github.com>
2026-07-28 19:33:26 +0100

    Step 9: add_bias_column

--- model.py
+++ model.py
@@ -75,8 +75,11 @@ def apply_standardizer(X, mean, std):
     # TODO: Return the scaled matrix (X - mean) / std via broadcasting.
     return (X-mean)/std
 
-# Step 9 - add_bias_column (not yet solved)
-# TODO: implement
+# Step 9 - add_bias_column
+def add_bias_column(X):
+    # TODO: Prepend a column of ones to a 2-D feature matrix X...
+    n = X.shape[0]
+    return np.hstack([np.ones((n, 1)), X])
 
 # Step 10 - make_shuffled_indices (not yet solved)
 # TODO: implement
